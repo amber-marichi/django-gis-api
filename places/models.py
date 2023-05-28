8cc47e630cee106810ecf91317738bf614d5ad02
Max Bilokur <bilmaxim@gmail.com>
2023-05-28 17:30:31 +0300

    initial place model

--- places/models.py
+++ places/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    geom = models.PointField(srid=4326)
+
+    class Meta:
+        db_table = "places"
+
+    def __str__(self) -> str:
+        return self.name
