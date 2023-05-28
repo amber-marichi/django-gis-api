5215cbfe83cbbee59721d707c63d32cc7f6b892d
Max Bilokur <bilmaxim@gmail.com>
2023-05-28 17:47:23 +0300

    added filter by query parameters by latitude and longitude to list view and serializer

--- places/serializers.py
+++ places/serializers.py
@@ -4,10 +4,17 @@ from places.models import Place
 
 
 class PlaceSerializer(serializers.ModelSerializer):
+    distance = serializers.DecimalField(
+        source="distance.km",
+        max_digits=10,
+        decimal_places=2,
+        required=False,
+        read_only=True
+    )
 
     class Meta:
         model = Place
-        fields = ("id", "name", "description", "geom")
+        fields = ("id", "name", "description", "geom", "distance")
 
     def validate_geom(self, value):
         if not -90 <= value.y <= 90:
--- places/views.py
+++ places/views.py
@@ -1,9 +1,45 @@
 from rest_framework import generics
+from rest_framework.exceptions import ValidationError
+from django.contrib.gis.geos import Point
+from django.contrib.gis.db.models.functions import Distance
+
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+from drf_spectacular.types import OpenApiTypes
 
 from places.models import Place
 from places.serializers import PlaceSerializer
 
 
 class ListCreatePlaces(generics.ListCreateAPIView):
-    queryset = Place.objects.all()
     serializer_class = PlaceSerializer
+
+    def get_queryset(self):
+        query = Place.objects.all()
+        longitude = self.request.query_params.get("lng")
+        latitude = self.request.query_params.get("lat")
+
+        if latitude and longitude:
+            try:
+                pnt = Point(float(longitude), float(latitude), srid=4326)
+            except ValueError:
+                raise ValidationError(detail="Invalid Params")
+            query = query.annotate(distance=Distance("geom", pnt)).order_by("distance")[:1]
+
+        return query
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name="lat",
+                type=OpenApiTypes.STR,
+                description="Enter latitude of coordinates",
+            ),
+            OpenApiParameter(
+                name="lng",
+                type=OpenApiTypes.STR,
+                description="Enter longitude of coordinates",
+            ),
+        ]
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
