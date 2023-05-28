804912d618dba1cd939c9c55dc7212343630042c
Max Bilokur <bilmaxim@gmail.com>
2023-05-28 17:42:22 +0300

    added validation for point coordinates

--- places/serializers.py
+++ places/serializers.py
@@ -8,3 +8,10 @@ class PlaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Place
         fields = ("id", "name", "description", "geom")
+
+    def validate_geom(self, value):
+        if not -90 <= value.y <= 90:
+            raise serializers.ValidationError("invalid latitude in coordinates")
+        if not -180 <= value.x <= 180:
+            raise serializers.ValidationError("invalid longitude in coordinates")
+        return value
